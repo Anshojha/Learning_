49a816a84d3dfb03f2671e2d88712b3d697dc94c
anshojha <anshojha24@gmail.com>
2022-07-28 14:40:00 +0530

    Virtual Function

--- Pointers/p_54_polyMoprphism.cpp
+++ Pointers/p_54_polyMoprphism.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int main()
 {
-           
+    // PolyMorphism :- One name and multiple form
+    // eg :- function overloading and pointer overloading
+    // eg :- virtual function
+
+    /*
+    Polymorphism in cpp is of two types:
+    1. Compile time polymorphism
+       Complile time polymorphism is achived in using:
+       1.1 - Function overloading
+       1.2 - Operator overloading
+    2. Run time polymorphism 
+       Run time polymorphism is achived in using:
+       2.1 - Virtual function
+    */
  return 0;
 }
---
+++ Pointers/tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+  // DerivedClass * derived_class_pointer;
+    // derived_class_pointer=&obj_derived; 
+    // derived_class_pointer->var_derived=223;
