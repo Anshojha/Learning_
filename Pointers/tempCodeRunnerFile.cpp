c72bf89293fddc932baaca32100ada135397797f
anshojha <anshojha24@gmail.com>
2022-07-30 14:44:48 +0530

    Virtual Function

--- Pointers/tempCodeRunnerFile.cpp
+++ Pointers/tempCodeRunnerFile.cpp
@@ -1,3 +1,6 @@
-  // DerivedClass * derived_class_pointer;
-    // derived_class_pointer=&obj_derived; 
-    // derived_class_pointer->var_derived=223;
+   void display()
+    {
+        cout << "This is an amazing text tutorial with title " << title << endl;
+        cout << "Rating of this text tutorial is: " << rating << "Out of 5 stars" << endl;
+        cout << "Number of words in this text tutorial is " << words << "words." << endl;
+    }
