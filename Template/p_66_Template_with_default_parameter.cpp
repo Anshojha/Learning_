11078be3242b1872fa1eaf15c6e4f6f0df2522d9
anshojha <anshojha24@gmail.com>
2022-08-24 18:05:34 +0530

    Template with default parameter

---
+++ Template/p_66_Template_with_default_parameter.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+template <class T1 = int, class T2 = float, class T3 = char>
+class Harry
+{
+public:
+    T1 a;
+    T2 b;
+    T3 c;
+    Harry(T1 x, T2 y, T3 z)
+    {
+        a = x;
+        b = y;
+        c = z;
+    }
+    void display()
+    {
+        cout << "The value of a is " << a << endl;
+        cout << "The value of b is " << b << endl;
+        cout << "The value of c is " << c << endl;
+    }
+};
+
+int main()
+{
+    Harry<> h(4, 4.67, 'd');
+    h.display();
+    cout << endl;
+    // Below is the different way of doing the the same as above
+    Harry<int, char, float> g(4, 'd', 'B');
+    // Harry <int , char , float> g(4,'d','B');   Here it print the ASCII value of B and wouldn't through the error
+    g.display();
+    return 0;
+}
