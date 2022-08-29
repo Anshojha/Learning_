03c4ab95cd0fb2bb8784f8eb360ea014f1324da5
anshojha <anshojha24@gmail.com>
2022-08-29 23:41:49 +0530

    STL

---
+++ Template/p_69_standred_library_template.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{  
+    // STL is used because its a good idea not to reinvent the wheel.
+    // STL - standered Template laibrary
+    // Components of STL - 1 . Containers - Which store the data and use template class
+    //                     2 . Algoruthms - set of procedures eg sortig , searching use template function
+    //                     3 . Iterators - objects to point a element in a container and handeled just like a pointer also connect alogorithm with containers       
+ return 0;}
