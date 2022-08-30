96fbe7487a6d4545525ce307f9057090e8a8b23d
anshojha <anshojha24@gmail.com>
2022-08-30 16:50:38 +0530

    STL

---
+++ Template/p_70_Containers_in_stl.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    /*
+
+    STL   =       Containers          +          Algorithm             +          Iterators
+            (Object which store data)   (procedure to process data)      (Objects to point an element in containers)
+
+    Continaers are of three types:
+    1. Sequence Containers  -->  Data stored in linear fashion eg Vector , List , Dequeue etc
+
+    2. Associate Containers --> Designed in a way to acees the data faster or direct access  eg map , set , multimap. multiset etc
+
+    3. Derived Containers --> Real world modeling or handeling  eg stack , queue , priority queue  etc
+
+
+     When to use which?
+     Sequence Containers
+     1. Vector --> Random access of data is faster and middle insertion/deletion is slow but insertion usually fast at the end
+
+
+
+
+    */
+
+    return 0;
+}
