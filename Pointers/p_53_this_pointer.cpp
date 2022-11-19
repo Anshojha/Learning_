a7913a55a84b1305ebc8ea9ae8226211978ec700
anshojha <anshojha24@gmail.com>
2022-11-19 23:21:52 +0530

    Pointer

--- Pointers/p_53_this_pointer.cpp
+++ Pointers/p_53_this_pointer.cpp
@@ -5,7 +5,7 @@ class A{
     int a;
     public:
     void setData(int a){
-      //a =a;   this give the garbage value without any error message
+      // a =a;  // this give the garbage value without any error message
       this->a=a;  // This is called this pointer used in this situation
     }
     void getData(){
@@ -16,8 +16,12 @@ class A{
 int main()
 {
     //this is a keyword which is pointer which points to the objects which invokes the member function
-   A a;
-   a.setData(3);
-   a.getData();        
+   A a,a2,a3;
+   a.setData(4);
+   a.getData();  
+   a2.setData(5);
+   a2.getData();  
+   a3.setData(6);
+   a3.getData();        
  return 0;
 }
